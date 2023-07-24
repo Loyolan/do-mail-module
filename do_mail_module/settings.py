5475a4b374b47b29a99ba4cba432982376e01cce
Kiady Loyolan <loyolan-akata@goavana.com>
2023-07-24 12:22:05 +0300

    Test(Connexion): Test connexion creation

--- do_mail_module/settings.py
+++ do_mail_module/settings.py
@@ -123,6 +123,14 @@ DATABASES = {
         'NAME': os.environ.get('PG_DB','postgres'),
         'PORT': os.environ.get('PG_PORT','5432'),
         'HOST': os.environ.get('PG_HOST','localhost'), # uses the container if set, otherwise it runs locally
+    },
+    'test': {
+        'ENGINE': os.environ.get('DB_DRIVER','django.db.backends.postgresql'),
+        'USER': os.environ.get('PG_USER'),
+        'PASSWORD':os.environ.get('PG_PASSWORD'),
+        'NAME': os.environ.get('PG_DB_TEST'),
+        'PORT': os.environ.get('PG_PORT','5432'),
+        'HOST': os.environ.get('PG_HOST','localhost'),
     }
 }
 
--- mail_client/tests.py
+++ mail_client/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from mail_client.models import Connexion
+import json
 
-# Create your tests here.
+class CreateConnexionViewTestCase(TestCase):
+    def test_create_connexion(self):
+        # Create Client DRF
+        client = APIClient()
+
+        # Data for creating Connexion
+        data = {
+            'mail_address': 'user@example.com',
+            'domaine': 'example.com',
+            'port': 587,
+            'password': 'password'
+        }
+
+        # Launch test
+        response = client.post('/api/mail_clients/create-connexion/', json.dumps(data), content_type='application/json')
+        # Response code should be 201
+        self.assertEqual(response.status_code, 201)
+
+        # Verify connexion created
+        connexion = Connexion.objects.get(mail_address='user@example.com')
+        self.assertEqual(connexion.domaine, 'example.com')
+        self.assertEqual(connexion.port, 587)
+
